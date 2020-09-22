d96b2d2868250d479be74f4f6ca359e6d459becc
NCompass-TV <56720009+NCompass-TV@users.noreply.github.com>
2020-09-22 19:29:33 +0800

    edit crontab via shell

--- shell/on-demand.sh
+++ shell/on-demand.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 # On Demand Script in case there are commands that needs to be run by force.
 # Set Permission
+
+echo 0 0 * * * ./home/pi/n-compasstv/pi-server/shell/remote-update.sh | crontab -
