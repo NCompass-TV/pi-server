40a35bdf526fad5f94ab1e83acc386bed041bd1d
earlvhin <earlgabuat@gmail.com>
2020-11-15 22:55:00 +0800

    Turn Off Player then Reboot

--- api/routes/remote-update.js
+++ api/routes/remote-update.js
@@ -13,20 +13,35 @@ initiatePiRestart = async (socket_server) => {
 	try {
 		const off = await shutdownPlayer();
 		console.log('#initiatePiRestart: ', off);
+		const restart = await restartPi();
+		console.log('#initiatePiRestart: ', restart);
     } catch (err) {
         console.log('#initiatePiRestart:', err)
     }
 }
 
 shutdownPlayer = async () => {
-	console.log('Shutting Down Player')
+	console.log('Turning Off Player')
     return new Promise((resolve, reject) => {
-        exec(`gnome-terminal -- pm2 stop all; echo 'Restarting in 5'; sleep 5; sudo reboot now`, (err, stdout, stderr) => {
+        exec(`gnome-terminal -- pm2 stop all`, (err, stdout, stderr) => {
             if (err) {
               console.log(err)
               reject(err)
             }
             resolve('Player is now off, Pi Rebooting', stdout);
+        });
+    })
+}
+
+restartPi = async () => {
+	console.log('Pi Restarting')
+    return new Promise((resolve, reject) => {
+        exec(`sudo reboot now`, (err, stdout, stderr) => {
+            if (err) {
+              console.log(err)
+              reject(err)
+            }
+            resolve('Pi Restart Gracefully', stdout);
         });
     })
 }
