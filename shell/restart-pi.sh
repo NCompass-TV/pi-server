91011df39536b47b5ef6d46b2f4a2cca38357e4b
earlvhin <59974059+earlvhin@users.noreply.github.com>
2021-01-16 00:47:05 -0600

    Graceful shutdown of services

--- shell/restart-pi.sh
+++ shell/restart-pi.sh
@@ -1,12 +1,15 @@
 #!/bin/sh
 
 echo 'Turning off Player';
-pm2 stop all;
+pm2 stop npm;
+
+echo 'Turning off Pi Server'
+pm2 stop app;
 
 echo 'Flushing PM2 Logs'
 pm2 flush;
 
-echo 'Player is turned off, Restarting Pi ...';
+echo 'Services are turned off, Restarting Pi ...';
 sleep 1;
 echo '5';
 sleep 1;
