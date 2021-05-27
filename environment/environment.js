6ca877967e37327cdae7e9ac3ec0daae9da53423
earlvhin <earlgabuat@gmail.com>
2021-05-27 13:25:45 +0800

    prod endpoint

--- environment/environment.js
+++ environment/environment.js
@@ -1,6 +1,6 @@
 const environment = {
-    api_base_url: 'https://stgapi.n-compass.online/api',
-    socket_url: 'https://stgsocket.n-compass.online',
+    api_base_url: 'https://nctvapi.n-compass.online/api',
+    socket_url: 'https://nctvsocket.n-compass.online',
     kafka_url: '52.73.23.100:9092'
 }
 
